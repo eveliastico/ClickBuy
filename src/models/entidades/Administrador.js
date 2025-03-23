bc475be1896db8656adb4ffa77a3277df26da30e
Melito <premiumm9@gmail.com>
2025-03-23 14:34:07 -0700

    av

--- src/models/entidades/Administrador.js
+++
@@ -1,9 +0,0 @@
-export class Administrador extends Usuario {
-    constructor(id, nombre, correoElectronico, contrasena) {
-        super(id, nombre, correoElectronico, contrasena, 'Administrador');
-    }
-
-    toString() {
-        return `${super.toString()}`;
-    }
-}
